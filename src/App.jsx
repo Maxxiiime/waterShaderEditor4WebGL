4ef117a4e709b1dd303019e940f0cbc2b5c43d17
Maxxiiime <max2002guillon@gmail.com>
2024-04-28 21:56:39 +0200

    Edit OrbitControls

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Environment } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { useThree } from '@react-three/fiber';
+import * as THREE from 'three';
 
 import Waves from './Waves'
 
 export default function App() {
 
+  useThree(({ camera }) => {
+    camera.rotation.set(THREE.MathUtils.degToRad(30), 0, 0);
+  });
+
+
+
   return (
     <>
-      <OrbitControls makeDefault/>
+      <Perf position="top-left" />
+      <OrbitControls 
+        makeDefault
+        enableDamping 
+        enableZoom={false}  
+      />
+      <ambientLight intensity={Math.PI / 2} />  
 
-      <Waves/>      
+       
+      <Environment
+        background
+        files='./img/syferfontein_6d_clear_puresky_2k.hdr'
+      />  
+
+      <Waves />
+
+
+      
     </>
   )
 }
