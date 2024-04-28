a6251b0ad161d1e12d65c3042d0b9b651c39b407
Maxxiiime <max2002guillon@gmail.com>
2024-04-28 21:53:55 +0200

    Edit camera position

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
 import App from './App'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
         <Canvas
+
             camera={ {
                 fov: 45,
                 near: 0.2,
-                far: 200,
-                position: [2, 1, 2],
+                far: 100,
+                position: [0, 13, 0],
             } }
         >
             <App />
-
         </Canvas>
 )
